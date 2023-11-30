0f26853cbcd2b7d73eb42a420d07da3d00895d5e
Adarsh Kumar <adarshkumar20012704@gmail.com>
2023-11-30 11:48:59 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,8 @@ const student_routes = require('./routes/student.routes');
 const faculty_routes = require('./routes/faculty.routes');
 const admin_routes = require('./routes/admin.routes');
 
+const is_student_authentic = require('./controllers/student.controller')
+
 
 // middlewares
 app.use(express.urlencoded({ extended: true }));    // to access data send through html forms
@@ -24,6 +26,10 @@ app.use('/admin', admin_routes);
 app.use('/student', student_routes);
 app.use('/faculty', faculty_routes);
 
+app.get('/unauthorised', (req, res)=>{
+    // returning a page which displays '401 Unauthorised Accesss!' and below will be a buthon to redirect to login page
+})
+
 
 app.get('/', (req, res)=>{
     // home route stuff
@@ -37,9 +43,37 @@ app.get('/', (req, res)=>{
 
     // if not logged in -> redirect to login page
     
-    // if role == 'admin' -> authorize person -> redirect to /admin
-    // if role == 'student' -> authorize person -> redirect to /student
-    // if role == 'faculty' -> authorize person -> redirect to /faculty
+    // set these keys in cookies in browser duing login and signup
+    const token = req.cookies.login;
+    const role = res.cookies.role;
+
+    /* -------- if role == 'student' -> authorize person -> redirect to /student -------- */
+    if(role=='student'){
+        if(is_student_authentic(token)){
+            res.redierct('/student');
+        }
+        else{
+            // removing cookies and redirecting to unauthorised page
+            res.cookie('login', '');
+            res.cookie('role', '');
+            res.redirect('/unauthorised'); // making unauthorised page
+        }
+    }
+    /* -------- if role == 'faculty' -> authorize person -> redirect to /faculty -------- */
+    else if(role=='faculty'){
+        // stuff
+    }
+    /* --------   if role == 'admin' -> authorize person -> redirect to /admin   -------- */
+    else if(role=='admin'){
+        // stuff
+    }
+    else{
+        res.status(404).json({
+            message: 'Nothing found!'
+        })
+    }
+
+
     
     // * check role from cookies
     // * after getting role, check for proper authenticity
